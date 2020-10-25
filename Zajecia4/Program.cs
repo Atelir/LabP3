c465fc3ba1a9d33d4be36ce6c16dee3828949237
Atelir <atelir911@gmail.com>
2020-10-25 10:08:14 +0100

    test

---
+++ Zajecia4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zajecia4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
