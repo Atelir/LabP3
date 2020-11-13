94ca5846d08652e28b036ef79b6b55f5c78b3fb8
Atelir <atelir911@gmail.com>
2020-11-13 17:56:50 +0100

    fin

---
+++ Zadanie4/Zadanie4/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Zadanie4
+{
+
+    public class Ksiazka
+    {
+        public string Tytul;
+        public string Autor;
+
+        public int NrRegalu = 0;
+        public int NrPolki = 0;
+        public int NrMiejscaNaPolce = 0;
+
+
+        public Ksiazka(string tytul, string autor, int nrRegalu, int nrPolki, int nrMiejscaNaPolce)
+        {
+            Tytul = tytul;
+            Autor = autor;
+            NrRegalu = nrRegalu;
+            NrPolki = nrPolki;
+            NrMiejscaNaPolce = nrMiejscaNaPolce;
+        }
+
+    }
+    class Program
+    {
+
+        static void WypelnijRegal(Ksiazka[,,] Regal)
+        {         
+            Ksiazka ksiazka = new Ksiazka("Elo", "test", 1, 1, 1);
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    for (int k = 0; k < 10; k++)
+                    {
+                        Regal[i, j, k] = ksiazka;
+                    }
+                }
+            }
+
+            Ksiazka a = new Ksiazka("Chce zdac p3", "Ja", 2, 1, 5);
+
+            Regal[2,1,5] = a;
+
+        }
+
+        static void WyszukanieNazwy(Ksiazka[,,] Regal,string sprawdzenie)
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    for (int k = 0; k < 10; k++)
+                    {
+
+                        if(Regal[i,j,k].Autor.Contains(sprawdzenie, StringComparison.OrdinalIgnoreCase) || Regal[i, j, k].Tytul.Contains(sprawdzenie, StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.WriteLine($"Podana książka znajduje się na: Regale:{Regal[i,j,k].NrRegalu}, Półce: {Regal[i, j, k].NrPolki} oraz na Miejscu: {Regal[i, j, k].NrMiejscaNaPolce}");
+                            return;
+                        }
+
+                    }
+
+                }
+            }
+            Console.WriteLine("Brak wpisanego wyrazu w tytule lub autorze ksiazki");
+        }
+
+        static void Main(string[] args)
+        {
+
+            Ksiazka[,,] Regal = new Ksiazka[3, 6, 10];
+
+            WypelnijRegal(Regal);
+
+            Console.WriteLine("Wpisz string: ");
+            string sprawdzenie = Console.ReadLine();
+
+            WyszukanieNazwy(Regal, sprawdzenie);
+        }
+    }
+}
