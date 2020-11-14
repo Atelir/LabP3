e7e52bd69a2b7ccc0aab937216e446586c0e98d0
Atelir <atelir911@gmail.com>
2020-11-14 23:09:28 +0100

    te

--- Kolos1/Program.cs
+++ Kolos1/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace Kolokwium1
+namespace Kolos1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World!");
         }
     }
 }
