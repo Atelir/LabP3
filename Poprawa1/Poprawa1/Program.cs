c594bb048c9b855fd25fdbf6b21dd805db4189eb
Atelir <atelir911@gmail.com>
2020-12-18 22:52:22 +0100

    test

---
+++ Poprawa1/Poprawa1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Poprawa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
