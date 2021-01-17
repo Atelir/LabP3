a0db397f4b2cb0e4cd9ad671dfc29906e9077067
Atelir <atelir911@gmail.com>
2021-01-17 13:34:24 +0100

    Zadanie 1

--- Poprawa2/Poprawa2/Program.cs
+++ Poprawa2/Poprawa2/Program.cs
@@ -4,9 +4,114 @@ namespace Poprawa2
 {
     class Program
     {
+
+        static void paginujTresc(int nrStrony, int iloscElem)
+        {
+            int[] tab = new int[1000];
+            
+
+            for (int i = 0; i < 1000; i++)
+            {
+                tab[i] = i+1;
+            }
+
+            int pierwszyIndex = (nrStrony-1)* iloscElem;
+            int ostatniIndex = pierwszyIndex + iloscElem;
+
+            for (int i = pierwszyIndex; i < ostatniIndex; i++)
+            {
+
+                Console.Write($"{tab[i]},");
+
+
+            }
+        }
+
+        class Konto
+        {
+
+            private string login;
+            private string haslo;
+            DateTime OstatniaAktywnosc;
+            bool CzyAktywny;
+            string Skrzynka;
+
+            public Konto(string login, string haslo)
+            {
+                this.login = login;
+                this.haslo = haslo;
+            }
+
+            public string Login 
+            {
+                set { login = value; }  
+            }
+
+            public string Haslo
+            {
+
+                set { haslo = value; }
+
+            }
+
+            public void nowaWiadomosc(string tekstWiadomosci)
+            {
+                if (CzyAktywny)
+                {
+                   Console.WriteLine(tekstWiadomosci);
+                }
+                else
+                {
+                    if (Skrzynka == null)
+                    {
+                        Skrzynka = tekstWiadomosci;
+                    }
+                    else
+                    {
+
+                        Skrzynka += Environment.NewLine;
+                        Skrzynka += tekstWiadomosci;
+                    }
+                }
+            }
+
+
+        }
+
+        static void oplatTab(double[] tab, double balans) {
+
+            double srednia = 0;
+
+            for (int i = 0; i < 12; i++)
+            {
+                srednia += tab[i];
+            }
+            
+            
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Zadanie 1
+            Console.WriteLine("Podaj nr strony");
+            int nrStrony = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj ilosc elementów");
+            int iloscElem = Int32.Parse(Console.ReadLine());
+            paginujTresc(nrStrony, iloscElem);
+
+
+            //Zadanie 2
+            double[] oplaty = new double[12];
+            Random rnd = new Random();
+            for (int i = 0; i < 12; i++)
+            {
+                oplaty[i] = rnd.NextDouble() * (200 - 50) + 50;
+            }
+
+
         }
+
+
+
     }
 }
