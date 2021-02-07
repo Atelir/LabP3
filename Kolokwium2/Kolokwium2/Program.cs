812bc376a7f29523d225734d708395b56e6d332e
Atelir <atelir911@gmail.com>
2021-02-07 10:38:38 +0100

    Zad 1

--- Kolokwium2/Kolokwium2/Program.cs
+++ Kolokwium2/Kolokwium2/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Linq;
 
 namespace Kolokwium2
 {
     class Program
     {
+
+        static private readonly Random _random = new Random();
+         static public int RandomNumber(int min, int max)
+        {
+
+             return _random.Next(min, max+1);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Zadanie 1
+
+            int[] tab = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+
+                tab[i] = RandomNumber(100,999);
+
+            }
+
+            Console.WriteLine("Podaj liczbe max 3 cyfrowa:");
+            string wpisanie = Console.ReadLine();
+
+            int count = 0;
+            
+            for (int i = 0; i < tab.Length; i++)
+            {
+                string test = tab[i].ToString();
+                if (test.StartsWith(wpisanie) && count < 10)
+                {
+                    Console.WriteLine(test);
+                    count++;
+
+                }
+
+            }
+               
+
+
+
+
         }
     }
 }
