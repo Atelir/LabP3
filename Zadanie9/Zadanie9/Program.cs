345069c9080d8470d1048b069690e6182d77d160
Atelir <atelir911@gmail.com>
2021-02-14 11:23:04 +0100

    Poprawione Extension Method

--- Zadanie9/Zadanie9/Program.cs
+++ Zadanie9/Zadanie9/Program.cs
@@ -2,84 +2,89 @@
 
 namespace Zadanie9
 {
-    class Program
+    public class Uzytkownik
     {
 
-        public class Uzytkownik
+        public string Nazwa;
+        public int id { get; private set; }
+        private static int m_Counter = 0;
+        public Uzytkownik(string nazwa)
         {
-
-            public string Nazwa;
-            public int id { get; private set; }
-            private static int m_Counter = 0;
-            public Uzytkownik(string nazwa)
-            {
-                Nazwa = nazwa;
-                this.id = System.Threading.Interlocked.Increment(ref m_Counter);
-            }
-
-            
-
-
-            public void SubskrybujKanal(Kanal kanal)
-            {
-                kanal.IloscSubskrypcji();
-                kanal.Opublikowanofilm += Kanal_Opublikowanofilm;
-
-            }
-
-            private void Kanal_Opublikowanofilm(object sender, EventArgs e)
-            {
-                Console.WriteLine("Nowy film");
-                
-            }
+            Nazwa = nazwa;
+            this.id = System.Threading.Interlocked.Increment(ref m_Counter);
         }
 
 
-        public class Kanal
+
+
+        public void SubskrybujKanal(Kanal kanal)
         {
-            public string Nazwa;
-            public int Id = 0;
-            public int LicznikWyswitlen = 0;
-            private static int m_Counter = 0;
-            public int LicznikSub = 0;
-            public Kanal(string nazwa)
-            {
-                Nazwa = nazwa;
-                this.Id = System.Threading.Interlocked.Increment(ref m_Counter);
-
-            }
-
-            public event EventHandler Opublikowanofilm;
-
-            public void WyswietlFilm(int id) {
-
-                LicznikWyswitlen++;
-
-            }
-
-            public void OpublikujFilm()
-            {
-
-                Opublikowanofilm.Invoke(this, EventArgs.Empty);
-                
-            }
-
-            public void IloscSubskrypcji()
-            {
-
-                LicznikSub++;
-
-            }
-
-
-            public void ExtensionMethod(Kanal kanal)
-            {
-
-                Console.WriteLine($"Nazwa kanalu to: {kanal.Nazwa}, subskrybuje ten kanał aktualnie: {kanal.LicznikSub} osoby, ile wyswietlen {kanal.LicznikWyswitlen}");
-
-            }
+            kanal.IloscSubskrypcji();
+            kanal.Opublikowanofilm += Kanal_Opublikowanofilm;
 
         }
+
+        private void Kanal_Opublikowanofilm(object sender, EventArgs e)
+        {
+            Console.WriteLine("Nowy film");
+
+        }
+    }
+
+
+    public class Kanal
+    {
+        public string Nazwa;
+        public int Id = 0;
+        public int LicznikWyswitlen = 0;
+        private static int m_Counter = 0;
+        public int LicznikSub = 0;
+        public Kanal(string nazwa)
+        {
+            Nazwa = nazwa;
+            this.Id = System.Threading.Interlocked.Increment(ref m_Counter);
+
+        }
+
+        public event EventHandler Opublikowanofilm;
+
+        public void WyswietlFilm(int id)
+        {
+
+            LicznikWyswitlen++;
+
+        }
+
+        public void OpublikujFilm()
+        {
+
+            Opublikowanofilm.Invoke(this, EventArgs.Empty);
+
+        }
+
+        public void IloscSubskrypcji()
+        {
+
+            LicznikSub++;
+
+        }
+
+
+
+    }
+    public static class KanalExtension
+    {
+
+        public static void Wypisz(this Kanal kanal)
+        {
+            Console.WriteLine($"Nazwa kanału: {kanal.Nazwa} " +
+                $"Liczba wyświetleń filmów: {kanal.LicznikWyswitlen} " +
+                $"Liczba subskrypcji: {kanal.LicznikSub} ");
+        }
+    }
+
+    class Program
+    {
 
 
         static void Main(string[] args)
@@ -95,7 +100,7 @@ namespace Zadanie9
             uzytkownik3.SubskrybujKanal(kanal1);
             uzytkownik4.SubskrybujKanal(kanal1);
             kanal1.OpublikujFilm();
-            kanal1.ExtensionMethod(kanal1);
+            kanal1.Wypisz();
 
         }
     }
